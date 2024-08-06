bc005e53e5c323c7b1298a9c3c6cf4e294cbec35
daviaxs <cadwvk@gmail.com>
2024-08-06 18:08:26 -0300

    feat: add search and quick-search sections in home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,47 @@
 import { Header } from '@/shared/components/header/Header'
+import { Button } from '@/shared/components/ui/button'
+import { Input } from '@/shared/components/ui/input'
+import { Search } from 'lucide-react'
+import {
+  QuickSearchOption,
+  quickSearchOptions,
+} from './utils/quick-search-options'
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Header />
-    </div>
+
+      <div className="px-4 py-6">
+        <div>
+          <h1 className="text-xl font-bold">Olá, Davi!</h1>
+          <p className="text-[0.7rem] text-gray-300">Sexta, 2 de Fevereiro</p>
+        </div>
+
+        <div className="mt-6 flex w-full items-center justify-center gap-2">
+          <Input placeholder="Buscar" />
+
+          <Button>
+            <Search size={15} />
+          </Button>
+        </div>
+
+        <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+          {quickSearchOptions.map((option: QuickSearchOption) => (
+            <Button key={option.title} variant={'secondary'} className="gap-2">
+              <Image
+                src={option.imageUrl}
+                alt={option.title}
+                width={16}
+                height={16}
+              />
+
+              <span className="text-[0.7rem]">{option.title}</span>
+            </Button>
+          ))}
+        </div>
+      </div>
+    </>
   )
 }
