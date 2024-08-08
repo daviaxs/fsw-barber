25ef7b8e48ef699f68998433dc0711dcf53b9882
daviaxs <cadwvk@gmail.com>
2024-08-08 17:36:06 -0300

    feat: Update Header component to include a link to the home page

--- src/shared/components/header/Header.tsx
+++ src/shared/components/header/Header.tsx
@@ -4,19 +4,22 @@ import { Button } from '../ui/button'
 import { MenuIcon } from 'lucide-react'
 import { Sheet, SheetTrigger } from '../ui/sheet'
 import { Sidebar } from '../sidebar/Sidebar'
+import Link from 'next/link'
 
 export function Header() {
   return (
     <Card className="rounded-none border-none outline outline-1 outline-border">
       <CardContent className="flex items-center justify-between px-4 py-2">
-        <Image
-          src="/fsw-barber.svg"
-          alt="fsw barber"
-          height={18}
-          width={120}
-          priority
-          quality={100}
-        />
+        <Link href="/">
+          <Image
+            src="/fsw-barber.svg"
+            alt="fsw barber"
+            height={18}
+            width={120}
+            priority
+            quality={100}
+          />
+        </Link>
 
         <Sheet>
           <SheetTrigger asChild>
