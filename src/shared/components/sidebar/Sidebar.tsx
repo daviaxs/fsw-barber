f4bade2f576c282712960b578fbab7615dd7cc57
daviaxs <cadwvk@gmail.com>
2024-08-08 15:59:25 -0300

    feat: Add sign in with google

--- src/shared/components/sidebar/Sidebar.tsx
+++ src/shared/components/sidebar/Sidebar.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { CalendarIcon, HomeIcon, LogInIcon } from 'lucide-react'
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from '../ui/sheet'
 import { quickSearchOptions } from '@/app/(home)/utils/quick-search-options'
@@ -5,29 +7,79 @@ import Link from 'next/link'
 import { Avatar, AvatarImage } from '../ui/avatar'
 import { Button } from '../ui/button'
 import Image from 'next/image'
+import { signIn, signOut, useSession } from 'next-auth/react'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '../ui/dialog'
 
 export function Sidebar() {
+  const handleLoginWithGoogle = () => signIn('google')
+  const handleLogout = () => signOut()
+  const { data } = useSession()
+
   return (
     <SheetContent className="overflow-y-scroll [&::-webkit-scrollbar]:hidden">
       <SheetHeader>
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
+      {data?.user ? (
+        <div className="flex items-center gap-2 border-b border-solid py-6">
+          <Avatar className="outline outline-1 outline-purple-500">
+            <AvatarImage
+              src={data.user.image as string}
+              style={{
+                objectFit: 'cover',
+              }}
+            />
+          </Avatar>
 
-      <div className="flex items-center gap-2 border-b border-solid py-6">
-        <Avatar className="outline outline-1 outline-purple-500">
-          <AvatarImage
-            src="https://plus.unsplash.com/premium_photo-1671656349322-41de944d259b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHBlc3NvYXxlbnwwfHwwfHx8MA%3D%3D"
-            style={{
-              objectFit: 'cover',
-            }}
-          />
-        </Avatar>
-
-        <div>
-          <p className="font-bold">Davi Alves</p>
-          <p className="text-xs text-gray-400">davialves@gmail.com.io</p>
+          <div>
+            <p className="font-bold">{data.user.name}</p>
+            <p className="text-xs text-gray-400">{data.user.email}</p>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="flex items-center justify-between gap-2 border-b border-solid py-6">
+          <h2 className="font-bold">Olá, faça seu login!</h2>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button size={'icon'}>
+                <LogInIcon />
+              </Button>
+            </DialogTrigger>
+
+            <DialogContent className="w-[90%] rounded-lg">
+              <DialogHeader>
+                <DialogTitle className="text-center">
+                  Faça login na plataforma
+                </DialogTitle>
+                <DialogDescription className="text-center">
+                  Conecte-se usando sua conta do Google
+                </DialogDescription>
+              </DialogHeader>
+
+              <Button
+                variant={'outline'}
+                className="gap-2 font-bold"
+                onClick={handleLoginWithGoogle}
+              >
+                <Image
+                  src={'./google.svg'}
+                  alt="google"
+                  height={18}
+                  width={18}
+                />
+                Google
+              </Button>
+            </DialogContent>
+          </Dialog>
+        </div>
+      )}
 
       <div className="flex flex-col items-center gap-3 border-b border-solid py-5">
         <SheetClose asChild>
@@ -68,7 +120,11 @@ export function Sidebar() {
       </div>
 
       <div className="flex flex-col items-center gap-3 py-5">
-        <Button className="w-full justify-start gap-2" variant={'ghost'}>
+        <Button
+          className="w-full justify-start gap-2"
+          variant={'ghost'}
+          onClick={handleLogout}
+        >
           <LogInIcon size={18} />
           Sair da conta
         </Button>
