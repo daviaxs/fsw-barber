4581abd3dc218a2ae37f37c3e57613a2987285d5
daviaxs <cadwvk@gmail.com>
2024-08-10 12:17:22 -0300

    feat: add Calendar component in Sheet on ServiceItem component

--- src/app/barbershops/[id]/utils/components/ServiceItem.tsx
+++ src/app/barbershops/[id]/utils/components/ServiceItem.tsx
@@ -1,7 +1,18 @@
+'use client'
+
 import { Button } from '@/shared/components/ui/button'
+import { Calendar } from '@/shared/components/ui/calendar'
 import { Card, CardContent } from '@/shared/components/ui/card'
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/shared/components/ui/sheet'
 import { BarbershopService } from '@prisma/client'
 import Image from 'next/image'
+import { ptBR } from 'date-fns/locale'
 
 interface ServiceItemProps {
   service: BarbershopService
@@ -36,9 +47,49 @@ export function ServiceItem({ service }: ServiceItemProps) {
               }).format(Number(service.price))}
             </p>
 
-            <Button variant={'secondary'} size={'sm'}>
-              Reservar
-            </Button>
+            <Sheet>
+              <SheetTrigger asChild>
+                <Button variant={'secondary'} size={'sm'}>
+                  Reservar
+                </Button>
+              </SheetTrigger>
+
+              <SheetContent className="px-0 max-sm:w-full">
+                <SheetHeader>
+                  <SheetTitle>Fazer Reserva</SheetTitle>
+                </SheetHeader>
+
+                <div className="border-b border-solid py-5">
+                  <Calendar
+                    mode="single"
+                    locale={ptBR}
+                    styles={{
+                      head_cell: {
+                        width: '100%',
+                        textTransform: 'capitalize',
+                      },
+                      cell: {
+                        width: '100%',
+                      },
+                      button: {
+                        width: '100%',
+                      },
+                      nav_button_previous: {
+                        width: '32px',
+                        height: '32px',
+                      },
+                      nav_button_next: {
+                        width: '32px',
+                        height: '32px',
+                      },
+                      caption: {
+                        textTransform: 'capitalize',
+                      },
+                    }}
+                  />
+                </div>
+              </SheetContent>
+            </Sheet>
           </div>
         </div>
       </CardContent>
