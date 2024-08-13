749da78613319011125872d19938906482319141
daviaxs <cadwvk@gmail.com>
2024-08-13 17:01:31 -0300

    feat: add BookingSummary component

---
+++ src/shared/components/booking-summary/BookingSummary.tsx
@@ -0,0 +1,46 @@
+import { Card, CardContent } from '@/shared/components/ui/card'
+import { Barbershop, BarbershopService } from '@prisma/client'
+import { format } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+
+interface BookingSummaryProps {
+  service: Pick<BarbershopService, 'name' | 'price'>
+  barbershop: Pick<Barbershop, 'name'>
+  selectedDate: Date
+}
+
+export function BookingSummary({
+  barbershop,
+  selectedDate,
+  service,
+}: BookingSummaryProps) {
+  return (
+    <Card>
+      <CardContent className="space-y-3 p-3">
+        <div className="flex items-center justify-between">
+          <h2 className="font-bold">{service.name}</h2>
+          <p className="text-sm font-bold">
+            {Intl.NumberFormat('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+            }).format(Number(service.price))}
+          </p>
+        </div>
+
+        <div className="flex items-center justify-between">
+          <h2 className="text-sm text-gray-400">Data</h2>
+          <p className="text-sm">
+            {format(selectedDate, "d 'de' MMMM", {
+              locale: ptBR,
+            })}
+          </p>
+        </div>
+
+        <div className="flex items-center justify-between">
+          <h2 className="text-sm text-gray-400">Barbearia</h2>
+          <p className="text-sm">{barbershop.name}</p>
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
