70b7b22c51ee23764ee5c4efb3261e025b660e04
daviaxs <cadwvk@gmail.com>
2024-08-13 15:58:07 -0300

    feat: add Bookings Page

---
+++ src/app/bookings/page.tsx
@@ -0,0 +1,56 @@
+import { getConcludedBookings } from '@/shared/actions/get-concluded-bookings'
+import { getConfirmedBookings } from '@/shared/actions/get-confirmed-bookings'
+import { BookingItem } from '@/shared/components/booking-item/BookingItem'
+import { Header } from '@/shared/components/header/Header'
+import { authOption } from '@/shared/lib/auth'
+import { getServerSession } from 'next-auth'
+import { notFound } from 'next/navigation'
+
+export default async function BookingsPage() {
+  const session = await getServerSession(authOption)
+
+  if (!session?.user) {
+    return notFound()
+  }
+
+  const confirmedBookings = await getConfirmedBookings()
+  const concludedBookings = await getConcludedBookings()
+
+  return (
+    <>
+      <Header />
+
+      <div className="space-y-3 p-5">
+        <h1 className="text-xl font-bold">Agendamentos</h1>
+
+        {confirmedBookings.length === 0 && concludedBookings.length === 0 && (
+          <p className="text-gray-400">Você não tem nenhum agendamento.</p>
+        )}
+
+        {confirmedBookings.length > 0 && (
+          <>
+            <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+              Confirmados
+            </h2>
+
+            {confirmedBookings.map((booking) => (
+              <BookingItem key={booking.id} booking={booking} />
+            ))}
+          </>
+        )}
+
+        {concludedBookings.length > 0 && (
+          <>
+            <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+              Finalizados
+            </h2>
+
+            {concludedBookings.map((booking) => (
+              <BookingItem key={booking.id} booking={booking} />
+            ))}
+          </>
+        )}
+      </div>
+    </>
+  )
+}
